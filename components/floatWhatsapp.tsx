e8fc47eebf7d5dd53dadd9f26409f54fad007a58
shubhsingh1515 <shubhsingh1515@gmail.com>
2025-11-17 16:12:45 +0530

    updated code

--- components/floatWhatsapp.tsx
+++ components/floatWhatsapp.tsx
@@ -1,18 +1,35 @@
 "use client";
 
-export default function FloatingWhatsApp() {
+export default function FloatingContacts() {
   return (
-    <a
-      href="https://api.whatsapp.com/send/?phone=&text=Hi&type=phone_number"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="fixed bottom-4 right-4 z-50  text-white w-14 h-14 rounded-full shadow-xl flex items-center justify-center"
-    >
-      <img
-        src="/logo/whatsapp.gif"
-        alt="WhatsApp"
-        className="w-10 h-10 rounded-full"
-      />
-    </a>
+    <div className="fixed bottom-4 right-4 z-50 flex flex-col items-center space-y-3">
+
+      {/* Call Button */}
+      <a
+        href="tel:+91XXXXXXXXXX"   // put your number here
+        className="   flex items-center justify-center "
+      >
+        <img
+          src="/logo/call.png"     // use any calling icon you have
+          alt="Call"
+          className="w-10 h-10"
+        />
+      </a>
+
+      {/* WhatsApp Button */}
+      <a
+        href="https://api.whatsapp.com/send/?phone=&text=Hi&type=phone_number"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-white w-14 h-14 rounded-full shadow-xl flex items-center justify-center"
+      >
+        <img
+          src="/logo/whatsapp.gif"
+          alt="WhatsApp"
+          className="w-10 h-10 rounded-full"
+        />
+      </a>
+
+    </div>
   );
 }
